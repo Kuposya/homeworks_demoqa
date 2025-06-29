2cab9afda504d3c3cda6a8e27301ea76eb12be31
Tatiana Volkovinskaia <kuposya@mail.ru>
2025-06-29 17:59:34 +0300

    added tests

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -27,9 +27,6 @@ public class RegistrationPage {
     public RegistrationPage openPage() {
         open("/automation-practice-form");
 
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
         return this;
     }
 
